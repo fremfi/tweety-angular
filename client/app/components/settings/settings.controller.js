a26ea2ea3750e9bdb6ce468c4d736430c91a3c80
fjmva <frederick.mfinanga@gmail.com>
2016-05-30 23:18:39 -0400

    fix #1

--- client/app/components/settings/settings.controller.js
+++ client/app/components/settings/settings.controller.js
@@ -1,5 +1,4 @@
 import Toastr from 'toastr';
-import defaultSettings from '../../common/defaultSettings';
 
 function SettingsController($state) {
 
@@ -9,7 +8,18 @@ function SettingsController($state) {
   vm.onReset = onReset;
 
   function onReset() {
-    vm.settings = defaultSettings;
+    vm.settings = {
+      numberOfColumns: '3',
+      columnOneName: 'AppDirect',
+      columnTwoName: 'laughingsquid',
+      columnThreeName: 'techcrunch',
+      columnOneTweetCount: 30,
+      columnTwoTweetCount: 30,
+      columnThreeTweetCount: 30,
+      startDate: null,
+      endDate: null,
+      themeColor: 'light-blue'
+    };
   }
 
   function onSave() {

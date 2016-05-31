86d7bf737ca4974c76d9e0e35059d930064c2d50
fjmva <frederick.mfinanga@gmail.com>
2016-05-30 23:35:36 -0400

    bug fix

--- client/app/app.controller.js
+++ client/app/app.controller.js
@@ -1,5 +1,3 @@
-import defaultSettings from './common/defaultSettings';
-
 function AppController () {
 
     const root = this;
@@ -10,7 +8,18 @@ function AppController () {
         root.settings = Object.assign({}, JSON.parse(savedSettings));
       }
       else {
-        root.settings = defaultSettings;
+        root.settings = {
+          numberOfColumns: '3',
+          columnOneName: 'AppDirect',
+          columnTwoName: 'laughingsquid',
+          columnThreeName: 'techcrunch',
+          columnOneTweetCount: 30,
+          columnTwoTweetCount: 30,
+          columnThreeTweetCount: 30,
+          startDate: null,
+          endDate: null,
+          themeColor: 'light-blue'
+        };
       }
     };
 }
